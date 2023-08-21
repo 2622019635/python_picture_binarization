6e6a719a088850758ced868f664a9dae0e0a6900
SZ\xujunce <xujunce@hitrontech.com>
2023-08-21 17:44:08 +0800

    It is already possible to binarize a single image.

--- picture_binarization_main.py
+++ picture_binarization_main.py
@@ -1,28 +1,45 @@
-import argparse
+from PIL import Image
 
-parse = argparse.ArgumentParser(description='picture binarization')
-parse.add_argument('-p', '--path', type=str, metavar='\b', help='Picture path.')
-parse.add_argument('-t', '--threshold', type=int, default=200,metavar='\b', choices=tuple(range(256)), help='Boundary values for black and white. [0-255]')
 
-args = parse.parse_args()
-# # 图片二值化
-# from PIL import Image
-# img = Image.open('test.jpg')
- 
-# # 模式L”为灰色图像，它的每个像素用8个bit表示，0表示黑，255表示白，其他数字表示不同的灰度。
-# Img = img.convert('L')
-# Img.save("test1.jpg")
- 
-# # 自定义灰度界限，大于这个值为黑色，小于这个值为白色
-# threshold = 200
- 
-# table = []
-# for i in range(256):
-#     if i < threshold:
-#         table.append(0)
-#     else:
-#         table.append(1)
- 
-# # 图片二值化
-# photo = Img.point(table, '1')
-# photo.save("test2.jpg")
+
+def picture_binarization(path, threshold, output_path):
+    try:
+        # check input
+        if not isinstance(path, str) or not isinstance(output_path, str):
+            raise ValueError("Input and output paths must be strings")
+        
+        if not isinstance(threshold, int) or threshold < 0 or threshold > 255:
+            raise ValueError("Threshold must be an integer between 0 and 255")
+        
+        # open image
+        img = Image.open(path)
+        
+        # convert to grayscale
+        img = img.convert('L')
+        
+        # create binarization table
+        table = []
+        for i in range(256):
+            if i < threshold:
+                table.append(0)
+            else:
+                table.append(1)
+        
+        # picture binarization
+        photo = img.point(table, '1')
+        
+        # save image
+        photo.save(output_path)
+    
+    except Exception as e:
+        print("Error:", e)
+    
+
+if __name__ == '__main__':
+		import argparse
+		parse = argparse.ArgumentParser(description='picture binarization')
+		parse.add_argument('-p', '--path', type=str, metavar='\b', help='Picture path.')
+		parse.add_argument('-t', '--threshold', type=int, default=200,metavar='\b', choices=tuple(range(256)), help='Boundary values for black and white. [0-255]')
+		parse.add_argument('-o', '--output_path', type=str, metavar='\b', help='Output path.')
+		args = parse.parse_args()
+		picture_binarization(args.path, args.threshold, args.output_path)
